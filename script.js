6df380ee7c730600a4616a96628058c9da786d3e
koneone2002 <katieo3507@gmail.com>
2020-05-05 16:02:56 -0700

    added voices selection

--- script.js
+++ script.js
@@ -70,10 +70,30 @@ function createBox(item) {
   // @todo -speak event
   main.appendChild(box);
 }
+// Store voices
+let voices = [];
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
 // Toggle text box
 toggleBtn.addEventListener('click', () =>
   document.getElementById('text-box').classList.toggle('show')
 );
+
+// Close Button
 closeBtn.addEventListener('click', () =>
   document.getElementById('text-box').classList.remove('show')
 );
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+getVoices();
